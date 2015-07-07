225475f2887c05fa6abd13d3c01acb24c4db7d13
KateWilkinson <kate.wilkinson@hotmail.com>
2015-07-07 18:58:24 +0100

    Added reset button; added classes to change colour dependent on temp

--- src/app.js
+++ src/app.js
@@ -1,29 +1,34 @@
 var thermostat = new Thermostat();
 
-// temp.innerHTML = thermostat.temperature + '°c'
-
 document.getElementById('up_button').onclick=function() {
   thermostat.increaseTemperature();
   document.getElementById('current_temperature').innerHTML = thermostat.temperature + '°c'
+  changeColour()
 };
 
 document.getElementById('down_button').onclick=function() {
   thermostat.decreaseTemperature();
   document.getElementById('current_temperature').innerHTML = thermostat.temperature + '°c'
+  changeColour()
 };
 
+document.getElementById('reset').onclick=function(){
+  thermostat.resetTemperature();
+  document.getElementById('current_temperature').innerHTML = thermostat.temperature + '°c'
+  changeColour()
+};
 
-// Thermostat.prototype.up = function() {
-//   var temp = document.getElementById('current_temperature');
-//   thermostat.increaseTemperature();
-//   temp.innerHTML = thermostat.temperature + '°c'
-// };
-
-// Thermostat.prototype.down = function() {
-//   var temp = document.getElementById('current_temperature');
-//   thermostat.decreaseTemperature();
-//   temp.innerHTML = thermostat.temperature + '°c'
-// };
+function changeColour() {
+  if (thermostat.colour == 'green') {
+    document.getElementById('current_temperature').className = 'green';
+    }
+  if (thermostat.colour == 'yellow') {
+    document.getElementById('current_temperature').className = 'yellow';
+    }
+  if (thermostat.colour == 'red') {
+    document.getElementById('current_temperature').className = 'red';
+  };
+};
 
 
 
